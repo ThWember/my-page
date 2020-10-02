6d84ee0144676c3fb48f30793b4b5b635f4ceab9
Thais Wemberlaine <thaiswemberlaine@gmail.com>
2020-10-02 15:55:27 -0300

    Updated JS

---
+++ script.js
@@ -0,0 +1,11 @@
+function linkedin() {
+    window.open("https://www.linkedin.com/in/thaiswemberlaine/");
+}
+
+function github() {
+    window.open("https://github.com/ThWember/");
+}
+
+function email() {
+    window.open("mailto:thaiswemberlaine@gmail.com");
+}
